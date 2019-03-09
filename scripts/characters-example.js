486bf56f455aafd324e0560097318fd1711e5273
Jonathan Ray <jonathanray17@gmail.com>
2019-03-08 19:29:39 -0500

    first commit, adds all the names to left side as buttons

---
+++ scripts/characters-example.js
@@ -0,0 +1,19 @@
+const characters = [
+    {
+    "url": "https://www.anapioficeandfire.com/api/characters/2", 
+    "name": "Walder", 
+    "gender": "Male", 
+    "culture": "", 
+    "born": "", 
+    "died": "", 
+    "titles": [""], 
+    "aliases": ["Hodor"], 
+    "father": "", 
+    "mother": "", 
+    "spouse": "", 
+    "allegiances": ["https://www.anapioficeandfire.com/api/houses/362"], 
+    "books": ["https://www.anapioficeandfire.com/api/books/1", "https://www.anapioficeandfire.com/api/books/2", "https://www.anapioficeandfire.com/api/books/3", "https://www.anapioficeandfire.com/api/books/5", "https://www.anapioficeandfire.com/api/books/8"], 
+    "povBooks": [], 
+    "tvSeries": ["Season 1", "Season 2", "Season 3", "Season 4", "Season 6"], 
+    "playedBy": ["Kristian Nairn"]
+    }, 
